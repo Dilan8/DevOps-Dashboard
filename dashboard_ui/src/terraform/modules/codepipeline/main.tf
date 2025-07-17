57152f026a7f79e94a275561edb2fa81708908bd
Dilan8 <vasandarajdilan64@gmail.com>
2025-07-17 23:08:32 +0530

    ecr permission

--- dashboard_ui/src/terraform/modules/codepipeline/main.tf
+++ dashboard_ui/src/terraform/modules/codepipeline/main.tf
@@ -75,6 +75,11 @@ resource "aws_iam_role_policy_attachment" "codebuild_attach" {
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
 }
 
+resource "aws_iam_role_policy_attachment" "codebuild_ecr_access" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+}
+
 resource "aws_codebuild_project" "react_build" {
   name          = "${var.project_name}-build"
   service_role  = aws_iam_role.codebuild_role.arn
