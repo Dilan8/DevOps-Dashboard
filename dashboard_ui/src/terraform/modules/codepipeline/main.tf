976312f3561aba31664aacd867570efe72f58928
Dilan8 <vasandarajdilan64@gmail.com>
2025-07-17 22:13:10 +0530

    directory name

--- dashboard_ui/src/terraform/modules/codepipeline/main.tf
+++ dashboard_ui/src/terraform/modules/codepipeline/main.tf
@@ -82,7 +82,7 @@ resource "aws_codebuild_project" "react_build" {
 
   source {
     type      = "CODEPIPELINE"
-    buildspec = "buildspec/buildspec.yml"
+    buildspec = "buildspec.yml"
   }
 
   environment {
